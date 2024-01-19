a788186bbaacece318de8233d814908d21c0e86d
msathya15 <88794906+msathya15@users.noreply.github.com>
2024-01-19 12:52:33 +0530

    Update car.service.ts

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -9,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CarService {
   private jsonUrl ="assets/data.json";
-  private apiUrl="https://localhost:4200/models";
+  
   carList:Car[]=[]
   private selectedCar = new BehaviorSubject<Car>(null);
   selectedCar$ = this.selectedCar.asObservable();
@@ -18,11 +18,7 @@ export class CarService {
   setSelectedCar(car:Car): void {
     this.selectedCar.next(car);
   }
-  constructor(private http: HttpClient) { }
-
-  getall() : Observable<any[]> {
-    console.log(this.http.any<any[]>(this.apiUrl));
-  }
+  constructor(private http: HttpClient) { }  
   getListOfCars() : Observable<Car[]> {
     console.log(this.http.get<Car[]>(this.jsonUrl));
     return this.http.get<Car[]>(this.jsonUrl);
